80db73aeed97c2980094c9d0839b2614c51255f5
EdgarValcarcel <edgarvalcarcel@hotmail.com>
2023-04-14 00:13:54 -0500

    user controller added, method Post, create user

---
+++ src/API/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Sat.Recruitment.Application.Common.Models;
+using Sat.Recruitment.Application.Users.Commands.CreateUser;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MediatR;
+using Sat.Recruitment.WebUI.Controllers;
+
+namespace Sat.Recruitment.API.Controllers;
+[Authorize]
+public class UserController : ApiControllerBase
+{  
+
+    [HttpPost]
+    public async Task<ActionResult<int>> Create(CreateUserCommand command)
+    {
+        return await Mediator.Send(command);
+    }
+}
